83a104f5347e1c6780dcd7f57784bbf0e37d0731
hashirk105 <107429236+hashirk105@users.noreply.github.com>
2023-10-07 18:50:29 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, "0.0.0.0", () => {
   const address = server.address();
   if (address) {
-    console.log("Server is running on : ", $(PORT));
+    console.log("Server is running on : ", (PORT));
   } else {
     console.error("Failed to get server address....");
   }
