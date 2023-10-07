9a53de97d40bd05c9dfb7386972f34d0307ea0a4
hashirk105 <107429236+hashirk105@users.noreply.github.com>
2023-10-07 13:39:44 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,11 @@ app.use(notFound);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+const server = app.listen(PORT, "0.0.0.0", () => {
+  const address = server.address();
+  if (address) {
+    console.log("Server is running on : ", $(PORT));
+  } else {
+    console.error("Failed to get server address....");
+  }
+});
